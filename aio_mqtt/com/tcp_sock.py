10ade160c369b4f2286dae72e2310db24f7e0dd8
SzakacsAdam <szakacs.adam.98@gmail.com>
2024-11-07 23:42:34 +0100

    MOV: com to package

--- aio_mqtt/com/tcp_sock.py
+++ aio_mqtt/com/tcp_sock.py
@@ -6,7 +6,6 @@ from asyncio import (
     StreamReaderProtocol,
     StreamWriter,
     Transport,
-    get_running_loop,
 )
 from collections.abc import Callable
 from socket import AddressFamily, SocketKind, socket
@@ -155,69 +154,3 @@ class TCPUnixSocket(_TcpSocket):
             server_hostname=None,
             ssl_handshake_timeout=None,
         )
-
-
-class MqttTCP:
-    def __init__(self) -> None:
-        self._loop: AbstractEventLoop = None  # type: ignore[assignment]
-        self._s_reader: StreamReader = None  # type: ignore[assignment]
-        self._s_writer: StreamWriter = None  # type: ignore[assignment]
-
-    async def _read_length(self) -> int:
-        multiplier: int = 1
-        value: int = 0
-        while True:
-            encoded_byte: bytes = await self._s_reader.read(1)
-            if 0 == len(encoded_byte):
-                raise ValueError()
-            encoded_int: int = int.from_bytes(encoded_byte, "big")
-            value += (encoded_int & 0x7F) * multiplier
-            if multiplier > 0x200000:
-                raise ValueError()
-            if (encoded_int & 0x80) == 0:
-                break
-            multiplier *= 0x80
-
-        return value
-
-    async def _read_loop(self) -> None:
-        ...
-
-    async def send_connect(self) -> None:
-        ...
-
-    async def send_publish(self) -> None:
-        ...
-
-    async def send_puback(self) -> None:
-        ...
-
-    async def send_pubrec(self) -> None:
-        ...
-
-    async def send_pubrel(self) -> None:
-        ...
-
-    async def send_pubcomp(self) -> None:
-        ...
-
-    async def send_subscribe(self) -> None:
-        ...
-
-    async def send_unsubscribe(self) -> None:
-        ...
-
-    async def send_pingreq(self) -> None:
-        ...
-
-    async def send_disconnect(self) -> None:
-        ...
-
-    async def send_auth(self) -> None:
-        ...
-
-    async def open(self) -> None:
-        ...
-
-    async def close(self) -> None:
-        ...
