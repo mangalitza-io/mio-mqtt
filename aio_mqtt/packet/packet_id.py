6b092e9f55d77893cba1a7e628782d1c564f26c3
SzakacsAdam <szakacs.adam.98@gmail.com>
2024-11-16 15:27:26 +0100

    ADD: temp packet id generator

---
+++ aio_mqtt/packet/packet_id.py
@@ -0,0 +1,36 @@
+from collections import deque
+from fileinput import close
+
+from aio_mqtt.types import Slots
+
+
+class PacketIdentifier:
+    # TODO OPTIMIZE
+    #  Temporary solution from wialon gmqtt
+    MAX: int = 0xF
+    __slots__: Slots = (
+        "_used_ids",
+        "_last_used_id",
+        "_max_plus_one",
+    )
+
+    def __init__(self) -> None:
+        self._used_ids: set[int] = set()
+        self._last_used_id: int = 0
+        self._max_plus_one: int = self.MAX + 1
+
+    def get_id(self) -> int:
+        if self.MAX == len(self._used_ids):
+            raise OverflowError()
+        while True:
+            self._last_used_id = (self._last_used_id + 1) % self._max_plus_one
+
+            if self._last_used_id not in self._used_ids:
+                self._used_ids.add(self._last_used_id)
+                return self._last_used_id
+
+    def release_id(self, packet_id: int) -> None:
+        try:
+            return self._used_ids.remove(packet_id)
+        except KeyError:
+            return None
